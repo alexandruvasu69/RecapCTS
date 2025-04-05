e3ad8e6d1b6f4c606c19a332ff959e83b4242f09
alexandruvasu69 <alexandru.vasu@gmail.com>
2025-04-05 17:42:15 +0300

    adaugat singleton eager main explicat

--- src/designPatterns/singleton/eager/Main.java
+++ src/designPatterns/singleton/eager/Main.java
@@ -2,6 +2,13 @@ package designPatterns.singleton.eager;
 
 public class Main {
     public static void main(String[] args) {
+        // se acceseaza prin getInstanta() nu prin new SingletonEager();
+        // se poate accesa de oriunde nu neaparat din main si datele vor fi aceleasi
+        // -> asta e important la singleton si de obicei e accesat din mai multe clase
+        SingletonEager instantaSingletonEager = SingletonEager.getInstanta();
 
+        // dupa accesarea cu getInstanta() se poate lucra ca si cu o clasa normala
+        // folosire de metode, setteri daca avem etc.
+        instantaSingletonEager.afisare();
     }
 }
